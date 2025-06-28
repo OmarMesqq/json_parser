df3cb3872399a0a0ee9b16f90b6967a4d6c90607
OmarMesqq <omarmsqt@gmail.com>
2025-06-28 17:43:19 -0300

    test: :test_tube: create test structure

--- run_tests.c
+++ run_tests.c
@@ -4,44 +4,12 @@
 #include "json_parser.h"
 #include "color_codes.h"
 
-static void print_result(int res, char* filename);
 static void run_test(const char* testName, const char* jsonFilePath, int expectedResult);
 
 int main() {
-    FILE* f1 = fopen("tests/step1/valid.json", "r");
-    FILE* f2 = fopen("tests/step1/invalid.json", "r");
-    if (!f1 || !f2) {
-        printf("failed to open json files!\n");
-        return -1;
-    }
-
-
-    TokenStream* ts1= tokenize(f1);
-    int j = 0;
-    while (j < ts1->size) {
-        printf("tokenList[%d]: %c\n", j, ts1->tokenList[j]);
-        j++;
-    }
-
-    int resf1 = parseJson(ts1);
-    print_result(resf1, "tests/step1/valid.json");
-
-    free_token_stream(ts1);
-    fclose(f1);
-    fclose(f2);
+    run_test("Step 1, valid JSON", "tests/step1/valid.json", 1);
+    run_test("Step 1, invalid JSON", "tests/step1/invalid.json", 0);
     return 0;
-}
-
-static void print_result(int res, char* filename) {
-    if (!filename) {
-        return;
-    }
-
-    if (res == 1) {
-        printf("Valid JSON: %s.\n", filename);
-    } else {
-        printf("Invalid JSON: %s.\n", filename);
-    }
 }
 
 static void run_test(const char* testName, const char* jsonFilePath, int expectedResult) {
