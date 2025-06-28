62d6c50532015fe607d07eda547ca58ca7147072
OmarMesqq <omarmsqt@gmail.com>
2025-06-28 17:24:17 -0300

    create color_codes.h

---
+++ color_codes.h
@@ -0,0 +1,8 @@
+#ifndef COLOR_CODES_H
+#define COLOR_CODES_H
+
+#define GREEN "\033[0;32m"
+#define RED "\033[31m"
+#define RESET_COLOR "\033[0m"
+
+#endif
--- run_tests.c
+++ run_tests.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "lexer.h"
 #include "parser.h"
+#include "json_parser.h"
+#include "color_codes.h"
 
 static void print_result(int res, char* filename);
+static void run_test(const char* testName, const char* jsonFilePath, int expectedResult);
+
 int main() {
     FILE* f1 = fopen("tests/step1/valid.json", "r");
     FILE* f2 = fopen("tests/step1/invalid.json", "r");
@@ -38,4 +42,24 @@ static void print_result(int res, char* filename) {
     } else {
         printf("Invalid JSON: %s.\n", filename);
     }
+}
+
+static void run_test(const char* testName, const char* jsonFilePath, int expectedResult) {
+    printf("Running test %s on file %s\n...", testName, jsonFilePath);
+    
+    FILE* fp = fopen(jsonFilePath, "r");
+    if (!fp) {
+        fprintf(stderr, RED "run_test: failed to open file %s on test %s\n" RESET_COLOR, jsonFilePath, testName);
+        return;
+    }
+    
+    int actualResult = ValidateJson(fp);
+
+    if (actualResult == expectedResult) {
+        printf(GREEN "Test %s on file %s passed.\n" RESET_COLOR, testName, jsonFilePath);
+    } else {
+        fprintf(stderr, RED "Test %s on file %s FAILED!\n" RESET_COLOR, testName, jsonFilePath);
+    }
+
+    fclose(fp);
 }
