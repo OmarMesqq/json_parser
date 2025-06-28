1c7291ae2eec7425e8da892ed2f06b5b6fccf442
OmarMesqq <omarmsqt@gmail.com>
2025-06-28 17:07:56 -0300

    add support for filepath in main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include "lexer.h"
 #include "parser.h"
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./json_parser <filename.json>\n");
+        return -1;
+    }
     return 0;
 }
