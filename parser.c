07657a813fecf44bc3b4e4e8c506f5f3326e3adc
OmarMesqq <omarmsqt@gmail.com>
2025-08-20 21:37:43 -0300

    refactor: :poop: parseString in a bad way

--- parser.c
+++ parser.c
@@ -43,6 +43,12 @@ int ParseJson(TokenStream* ts) {
                 return currentParseStatus;
             }
             break;
+        case STRING_START_END:
+            currentParseStatus = parseString(ts, &pos);
+            if (currentParseStatus == -1) {
+                return currentParseStatus;
+            }
+            break;
         default:
             fprintf(stderr, "ParseJson: unexpected token: %d (decimal), %c (char)\n", currentToken, currentToken);
             break;
@@ -62,6 +68,7 @@ static int parseObject(TokenStream* ts, int* pos) {
     (*pos)++;   // parse current BEGIN_OBJECT
 
     int foundObjectEnd = 0;
+    int parseStatus = 0;
     TOKEN currentToken = ts->tokenList[*pos];
     do {
         currentToken = ts->tokenList[*pos];
@@ -70,6 +77,12 @@ static int parseObject(TokenStream* ts, int* pos) {
         switch (currentToken) {
             case END_OBJECT:
                 foundObjectEnd = 1;
+                break;
+            case STRING_START_END:
+                parseStatus = parseString(ts, pos);
+                if (parseStatus == -1) {
+                    return parseStatus;
+                }
                 break;
             default:
                 fprintf(stderr, "parseObject: unexpected token: %d (decimal), %c (char)\n", currentToken, currentToken);
@@ -83,5 +96,34 @@ static int parseObject(TokenStream* ts, int* pos) {
     };
 
     fprintf(stderr, "Expected a } for closing object.");
+    return -1;
+}
+
+static int parseString(TokenStream* ts, int* pos) {
+    (*pos)++;   // parse current STRING_START_END
+
+    int foundMatchingEndDoubleQuote = 0;
+    TOKEN currentToken = ts->tokenList[*pos];
+    do {
+        currentToken = ts->tokenList[*pos];
+        (*pos)++;
+        
+        switch (currentToken) {
+            case STRING_START_END:
+                foundMatchingEndDoubleQuote = 1;
+                (*pos)++;   // mark current double quote as parsed
+                break;
+            default:
+                fprintf(stderr, "parseString: unexpected token: %d (decimal), %c (char)\n", currentToken, currentToken);
+                return -1;
+        }
+    }
+    while (currentToken != STRING_START_END && !foundMatchingEndDoubleQuote);
+
+    if (foundMatchingEndDoubleQuote) {
+        return 0;
+    };
+
+    fprintf(stderr, "Expected closing \" for closing string.");
     return -1;
 }
