2fa9639bcdda8cf6943bb82601821bdf7bb6bdf2
OmarMesqq <omarmsqt@gmail.com>
2025-08-20 21:37:02 -0300

    chore: :bulb: add comments specifying JSON chars in grammer

--- grammar.h
+++ grammar.h
@@ -2,13 +2,16 @@
 #define GRAMMAR_H
 
 typedef enum {
+  // structural chars
   BEGIN_ARRAY = '[',
   BEGIN_OBJECT = '{',
   END_ARRAY = ']',
   END_OBJECT = '}',
   NAME_SEPARATOR = ':',
   VALUE_SEPARATOR = ',',
+  // strings
   STRING_START_END = '"',
+  // literal names
   LITERAL_TRUE,
   LITERAL_FALSE,
   LITERAL_NULL,
