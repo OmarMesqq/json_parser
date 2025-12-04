74cb84a098d612db2662eda106fb4f76ad2ce1b3
OmarMesqq <omarmsqt@gmail.com>
2025-12-04 13:44:01 -0300

    add all tests

--- run_tests.c
+++ run_tests.c
@@ -9,10 +9,19 @@ int main() {
   run_test("Step 1, valid JSON", "tests/step1/valid.json", 0);
   run_test("Step 1, valid simple string JSON", "tests/step1/valid_simple_string.json", 0);
   run_test("Step 1, invalid JSON", "tests/step1/invalid.json", -1);
+
   run_test("Step 2, valid JSON", "tests/step2/valid.json", 0);
   run_test("Step 2, invalid JSON", "tests/step2/invalid.json", -1);
   run_test("Step 2, valid JSON 2", "tests/step2/valid2.json", 0);
   run_test("Step 2, invalid JSON 2", "tests/step2/invalid2.json", -1);
+
+  run_test("Step 3, valid JSON", "tests/step3/valid.json", 0);
+  run_test("Step 3, invalid JSON", "tests/step3/invalid.json", -1);
+
+  run_test("Step 4, valid JSON", "tests/step4/valid.json", 0);
+  run_test("Step 4, invalid JSON", "tests/step4/invalid.json", -1);
+  run_test("Step 4, valid JSON 2", "tests/step4/valid2.json", 0);
+
   return 0;
 }
 
